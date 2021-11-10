2a0c30e82e47607edfe515b6daee388f0a3ebdf9
wthhx4 <wthhx4@163.com>
2021-11-10 11:33:53 +0800

    System home page

--- gmall-product/src/main/java/com/hhx4/gmall/product/feign/WareFeignService.java
+++ gmall-product/src/main/java/com/hhx4/gmall/product/feign/WareFeignService.java
@@ -1,6 +1,22 @@
 package com.hhx4.gmall.product.feign;
 
+import com.hhx4.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient("gmall-ware")
 public interface WareFeignService {
 
-//TODO 远程调用
+    /**
+     * 1、R设计的时候可以加上泛型
+     * 2、直接返回我们想要的结果
+     * 3、自己封装解析结果
+     * @param skuIds
+     * @return
+     */
+    @PostMapping("/ware/waresku/hasStock")
+    R getSkusHasStock(@RequestBody List<Long> skuIds);
 }
